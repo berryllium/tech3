d90832bb35b0f3d85400e7a26ba7f4e6c418b2e2
berryllium <gorkundp@yandex.ru>
2020-04-09 00:23:03 +0300

    удалил лишнюю папку

--- public/admin/models/config/db.php
+++ public/admin/models/config/db.php
@@ -2,12 +2,12 @@
 define('SERVER', 'localhost');
 define('DRIVER', 'mysql');
 
-// define('DB', 'shop_tech2');
-// define('USERNAME', 'root');
-// define('PASSWORD', '');
+define('DB', 'shop_tech2');
+define('USERNAME', 'root');
+define('PASSWORD', '');
 
 
 
-define('DB', 'a0348706_shop_tech');
-define('USERNAME', 'a0348706_shop_tech');
-define('PASSWORD', '4904000qq');
+// define('DB', 'a0348706_shop_tech');
+// define('USERNAME', 'a0348706_shop_tech');
+// define('PASSWORD', '4904000qq');
--- tech-market-master/public/admin/models/config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SERVER', 'localhost');
-define('DRIVER', 'mysql');
-define('DB', 'shop_tech');
-define('USERNAME', 'root');
-define('PASSWORD', '');
